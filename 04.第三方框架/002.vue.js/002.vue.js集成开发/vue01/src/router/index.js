f4c999f70c427eade07849d58aa14dc5fc07cbe7
liheng <13688007165@139.com>
2022-02-13 11:57:44 +0800

    GO

--- 04.第三方框架/002.vue.js/002.vue.js集成开发/vue01/src/router/index.js
+++ 04.第三方框架/002.vue.js/002.vue.js集成开发/vue01/src/router/index.js
@@ -1,20 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld001 from '@/components/HelloWorld001'
-import HelloWorld002 from '@/components/HelloWorld002'
-import HelloWorld003 from '@/components/HelloWorld003'
-import HelloWorld004 from '@/components/HelloWorld004'
-import HelloWorld005 from '@/components/HelloWorld005'
-import HelloWorld006 from '@/components/HelloWorld006'
-import HelloWorld007 from '@/components/HelloWorld007'
-import HelloWorld008 from '@/components/HelloWorld008'
-import HelloWorld009 from '@/components/HelloWorld009'
-import HelloWorld010 from '@/components/HelloWorld010'
-import HelloWorld011 from '@/components/HelloWorld011'
 
 
 
-import Test001 from '@/components/demo001/Test001'
 
 Vue.use(Router)
 
@@ -22,15 +11,6 @@ export default new Router({
   routes: [
 	
 	
-	
-    {
-      path: '/test001',
-      name: 'Test001',
-      component: Test001
-    },
-    
-    
-    
     
     
     {
@@ -38,46 +18,11 @@ export default new Router({
       name: 'HelloWorld001',
       component: HelloWorld001
     },
-    {
-      path: '/helloWorld002',
-      name: 'HelloWorld002',
-      component: HelloWorld002
-    },
-    {
-      path: '/helloWorld003',
-      name: 'HelloWorld003',
-      component: HelloWorld003
-    },
-    {
-      path: '/helloWorld004',
-      name: 'HelloWorld004',
-      component: HelloWorld004
-    },
-    {
-      path: '/helloWorld005',
-      name: 'HelloWorld005',
-      component: HelloWorld005
-    },
-    {
-      path: '/helloWorld006',
-      name: 'HelloWorld006',
-      component: HelloWorld006
-    },
-    {
-      path: '/helloWorld007',
-      name: 'HelloWorld007',
-      component: HelloWorld007
-    },
-    {
-      path: '/helloWorld008',
-      name: 'HelloWorld008',
-      component: HelloWorld008
-    },
-    {
-      path: '/helloWorld009',
-      name: 'HelloWorld009',
-      component: HelloWorld009
-    },
+ 
+ 
+ 
+    /**
+    ,
     {
       path: '/helloWorld010',
       name: 'HelloWorld010',
@@ -88,5 +33,6 @@ export default new Router({
       name: 'HelloWorld011',
       component: HelloWorld011
     }
+    */
   ]
 })
