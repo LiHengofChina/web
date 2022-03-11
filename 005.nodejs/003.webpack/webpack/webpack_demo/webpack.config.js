626d4852560e331b753dd15945d754a9a06211f4
liheng <13688007165@139.com>
2022-03-11 21:00:35 +0800

    GO

--- 005.nodejs/003.webpack/webpack/webpack_demo/webpack.config.js
+++ 005.nodejs/003.webpack/webpack/webpack_demo/webpack.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  devtool: 'source-map',
+
+  devtool: 'source-map',  
+
+  //devtool: 'cheap-source-map',
+
   
   entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
   
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
+    path: __dirname + "/publicx",//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
   },
 
