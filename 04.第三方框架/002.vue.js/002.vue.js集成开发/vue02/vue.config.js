9af0544f7961d349b8f8c162549c3d0187ec8571
liheng <13688007165@139.com>
2022-03-12 22:11:40 +0800

    GO

--- 04.第三方框架/002.vue.js/002.vue.js集成开发/vue02/vue.config.js
+++ 04.第三方框架/002.vue.js/002.vue.js集成开发/vue02/vue.config.js
@@ -6,10 +6,14 @@ console.log("_________1________");
 let webpaakConfig;
 
 let env = process.env.ENV;
-console.log("_________env________" + env);
 
-//webpaakConfig = require(`./webpackConfig/${env}.js`)
-webpaakConfig = require(`./webpackConfig/development.js`)
+console.log("_________process________" + process);
+console.log("_________process.env________" + process.env);
+console.log("_________process.env.ENV________" + env);
+
+
+
+webpaakConfig = require(`./webpackConfig/${env}.js`);
 
 module.exports = webpaakConfig;
 
