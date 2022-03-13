324b670547ee9e679a4fa7b1be4770706b90de3d
liheng <13688007165@139.com>
2022-03-13 18:58:44 +0800

    GO

--- 04.第三方框架/002.vue.js/002.vue.js集成开发/vue02/webpackConfig/development.js
+++ 04.第三方框架/002.vue.js/002.vue.js集成开发/vue02/webpackConfig/development.js
@@ -12,13 +12,19 @@ function resolve(dir) {
 const port = 9528;
 
 module.exports = merge(webpackBaseConfig,{
-	productionSourceMap: false , // 生产环境是否生成 sourceMap 文件
+	productionSourceMap: false , 		// 生产环境是否生成 sourceMap 文件
 	devServer:{
-		host: "localhost",
-		port: port,
-		open: true,
+		host: "localhost",				//指定要使用的 host。
+		port: port, 					//指定监听请求的端口号：
+		//open: true,					//告诉 dev-server 在服务器已经启动后打开浏览器。设置其为 true 以打开你的默认浏览器。
+		open: {
+		       app: {
+		           name: 'google-chrome',
+		         }
+			  },
+		
 		client:{
-			overlay:{
+			overlay:{ 					//当出现编译错误或警告时，在浏览器中显示全屏覆盖。
 				warnings: false,
 				errors: true
 			}
@@ -29,19 +35,17 @@ module.exports = merge(webpackBaseConfig,{
 		
 		
 		
-		hot: true,
-		compress: true
-		,
-		proxy: require("./proxy.js")
+		hot: true,  					//启用 webpack 的 热模块替换 特性：
+		compress: true,					//启用 gzip compression：
+		
+		proxy: require("./proxy.js")	//代理配置
 	},
 	configureWebpack: {
 		plugins: []
 	},
+	
 	chainWebpack(config){
-
-		
-		
-		
+ 
 		
 		config.devtool("eval") //配置 source-map
 		
