09e158fd0b4a208f6fffccc88b750a1212a808bd
liheng_xiaomi <375461826@qq.com>
2024-07-31 08:12:50 +0800

    111

--- 04.第三方框架/002.vue.js/004.vue.js__app开发/worspace_vue/vue_app_001/src/router/index.js
+++ 04.第三方框架/002.vue.js/004.vue.js__app开发/worspace_vue/vue_app_001/src/router/index.js
@@ -1,26 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/components/HelloWorld.vue';
-import ApprovalForm from '@/components/ApprovalForm.vue';
-import LoginPage from '@/components/LoginPage.vue';
+
+
+
+const Home = () => import(/* webpackChunkName: "home" */ '@/components/HelloWorld.vue');
+const ApprovalForm = () => import(/* webpackChunkName: "approval" */ '@/components/ApprovalForm.vue');
+const LoginPage = () => import(/* webpackChunkName: "log" */ '@/components/LoginPage.vue');
+
+
 
 
 const routes = [
-{
-    path: '/',
-    name: 'Home',
-    component: Home
-},
-{
-    path: '/approval',
-    name: 'Approval',
-    component: ApprovalForm
-}, {
-    path: '/log',
-    name: 'LoginPage',
-    component: LoginPage,
-}
-
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/approval',
+        name: 'Approval',
+        component: ApprovalForm
+    }, {
+        path: '/log',
+        name: 'LoginPage',
+        component: LoginPage,
+    }
 ];
+
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
