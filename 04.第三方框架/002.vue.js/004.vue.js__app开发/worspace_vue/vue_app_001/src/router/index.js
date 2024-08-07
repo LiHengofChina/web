730666b063a1b8369c498f647c76b7fc9003b62f
liheng_xiaomi <375461826@qq.com>
2024-08-07 16:51:06 +0800

    111

--- 04.第三方框架/002.vue.js/004.vue.js__app开发/worspace_vue/vue_app_001/src/router/index.js
+++ 04.第三方框架/002.vue.js/004.vue.js__app开发/worspace_vue/vue_app_001/src/router/index.js
@@ -1,14 +1,14 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import IndexLayout from '@/views/home/IndexLayout.vue';
+import IndexLayout from '@/views/index/IndexLayout.vue';
 
 
 // ====================================================== 顶级页面组件
 // index页
-const HomePage = () => import(/* webpackChunkName: "home-page" */ '@/views/home/bottom_menu/HomePage.vue');
-const WorkstationPage = () => import(/* webpackChunkName: "workstation-page" */ '@/views/home/bottom_menu/WorkstationPage.vue');
-const MessageList = () => import(/* webpackChunkName: "message-list" */ '@/views/home/bottom_menu/MessageList.vue');
-const UserProfile = () => import(/* webpackChunkName: "user-profile" */ '@/views/home/bottom_menu/UserProfile.vue');
+const HomePage = () => import(/* webpackChunkName: "home-page" */ '@/views/index/bottom_menu/HomePage.vue');
+const WorkstationPage = () => import(/* webpackChunkName: "workstation-page" */ '@/views/index/bottom_menu/WorkstationPage.vue');
+const MessageList = () => import(/* webpackChunkName: "message-list" */ '@/views/index/bottom_menu/MessageList.vue');
+const UserProfile = () => import(/* webpackChunkName: "user-profile" */ '@/views/index/bottom_menu/UserProfile.vue');
 // 登陆页
 const LoginPage = () => import(/* webpackChunkName: "log" */ '@/views/login/LoginPage.vue');
 
